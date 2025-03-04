993b83b1482b315acae50c3dd947dc3df60eb045
kenwCoding <kenwwk960417code@gmail.com>
2025-03-05 04:03:47 +0800

    Added title to welcome page

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -5,7 +5,7 @@ export function Welcome() {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
-        <header className="flex flex-col items-center gap-9">
+        <header className="flex flex-col items-center gap-9"><h1 className="text-4xl font-bold text-gray-900 dark:text-white">My AI Project</h1>
           <div className="w-[500px] max-w-[100vw] p-4">
             <img
               src={logoLight}
